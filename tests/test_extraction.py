c0ddf965e954b0b71f3eeaf4cad94811db63a9b0
alexciechonski <alex.ciechonski@gmail.com>
2025-10-14 19:18:57 +0200

    fixed this test

--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -1,14 +1,14 @@
 import pytest
 import os
 from config import RAW_DATA_DIRECTORY, TEST_FRACTION
-from tests.helpers import list_device_files, sample_devices, validate_columns_consistent,validate_row_count
+from tests.helpers import list_device_dirs, sample_devices, validate_columns_consistent,validate_row_count, unpack_device
 from src.features.feature_extraction import ExtractionPipeline
 import pandas as pd
 
 @pytest.mark.integration
 def test_extraction():
     """Integration test for feature extraction across a subset of devices."""
-    device_files = list_device_files(RAW_DATA_DIRECTORY)
+    device_files = list_device_dirs(RAW_DATA_DIRECTORY)
     assert device_files, "No device files found in raw data directory."
 
     sampled_devices = sample_devices(device_files, frac=TEST_FRACTION)
@@ -18,11 +18,11 @@ def test_extraction():
 
     extractor = ExtractionPipeline()
     for device_file in sampled_devices:
-        pcap_path = os.path.join(RAW_DATA_DIRECTORY, device_file)
-        df = extractor.extract_features(str(pcap_path))
+        for pcap_path in unpack_device(device_file):
+            df = extractor.extract_features(pcap_path)
 
-        assert isinstance(df, pd.DataFrame), f"Output not DataFrame for {device_file}"
-        assert not df.empty, f"No features extracted for {device_file}"
+            assert isinstance(df, pd.DataFrame), f"Output not DataFrame for {device_file}"
+            assert not df.empty, f"No features extracted for {device_file}"
 
-        reference_cols = validate_columns_consistent(df, reference_cols)
-        validate_row_count(df, pcap_path)
+            reference_cols = validate_columns_consistent(df, reference_cols)
+            validate_row_count(df, pcap_path)
