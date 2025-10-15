e02fc6fc83120fe6bc4573c16e6a01b38e77ce42
alexciechonski <alex.ciechonski@gmail.com>
2025-10-15 14:07:42 +0200

    added fast extraction

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ CONFIG = load_config()
 FEATURE_MENU_PATH = CONFIG["feature_menu_path"]
 RAW_DATA_DIRECTORY = CONFIG['raw_data_directory']
 PREPROCESSED_DATA_DIRECTORY = CONFIG['preprocessed_data_directory']
+FAST_EXTRACTION_DIRECTORY = CONFIG['fast_extraction_directory']
 VALID_FEATURES_DIRECTORY = CONFIG['valid_features_directory']
 MODELS_DIRECTORY = CONFIG['models_directory']
 TIME_INTERVALS = CONFIG['time_intervals']
--- tests/test_extraction.py
+++ tests/test_extraction.py
@@ -3,10 +3,11 @@ import os
 from config import RAW_DATA_DIRECTORY, TEST_FRACTION
 from tests.helpers import list_device_dirs, sample_devices, validate_columns_consistent,validate_row_count, unpack_device
 from src.features.feature_extraction import ExtractionPipeline
+from src.features.fast_extraction import FastExtractionPipeline
 import pandas as pd
 
-@pytest.mark.integration
-def test_extraction():
+@pytest.mark.parametrize("extractor_class", [ExtractionPipeline, FastExtractionPipeline])
+def test_extraction(extractor_class):
     """Integration test for feature extraction across a subset of devices."""
     device_files = list_device_dirs(RAW_DATA_DIRECTORY)
     assert device_files, "No device files found in raw data directory."
@@ -16,7 +17,7 @@ def test_extraction():
 
     reference_cols = None
 
-    extractor = ExtractionPipeline()
+    extractor = extractor_class()  # instantiate whichever pipeline we’re testing
     for device_file in sampled_devices:
         for pcap_path in unpack_device(device_file):
             df = extractor.extract_features(pcap_path)
