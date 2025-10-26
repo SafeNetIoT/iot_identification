8ff0ef0af63dd602cbf71f735143a5730a9697d4
alexciechonski <101641507+alexciechonski@users.noreply.github.com>
2025-10-26 10:45:58 +0100

    Removed unused multiclass model test file

--- tests/test_multiclass_model.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from tests.helpers import _run_unseen_evaluation
-
-@pytest.mark.integration
-def test_unseen_multiclass(multiclass_model_under_test):
-    _run_unseen_evaluation(multiclass_model_under_test, multiclass_model_under_test.multi_predict)
